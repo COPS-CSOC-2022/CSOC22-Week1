29d421e0da80b2d69b16a3e1e86b3dca6b789895
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2022-06-02 16:05:19 +0530

    Bug Fix in Initialization

---
+++ js_files/helper.js
@@ -0,0 +1,75 @@
+// Block all the buttons
+function blocker() {
+    let letterButtons = document.querySelectorAll(".letters");
+    letterButtons.forEach((button) => {
+        button.disabled.true;
+    });
+    newGameContainer.classList.remove("hide");
+};
+
+
+// Random Word Generator from the given options
+function generateWord(levelInp) {
+    // console.log(levels["Easy"]);
+    level = levelInp;
+
+    letterContainer.classList.remove("hide");
+    userInput.innerText = "";
+    let wordArray = levels[level]["words"];
+    maxLooseCount = levels[level]["attempts"];
+
+    // console.log(wordArray);
+    // console.log(maxLooseCount);
+
+    chosenWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    chosenWord = chosenWord.toUpperCase();
+    let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
+    userInput.innerHTML = displayItem;
+
+};
+
+function createLetterButtons() {
+    for (let i = 65; i < 91; i++) {
+        let button = document.createElement("button");
+        button.classList.add("letters");
+        button.innerText = String.fromCharCode(i);
+        button.setAttribute('id', 'letter' + i);
+        button.addEventListener("click", () => {
+            let charArray = chosenWord.split("");
+            let dashes = document.getElementsByClassName("dashes");
+            if (charArray.includes(button.innerText)) {
+                charArray.forEach((char, index) => {
+                    if (char === button.innerText) {
+                        dashes[index].innerText = char;
+                        winCount += 1;
+                        if (winCount == charArray.length) {
+                            resultText.innerHTML = `<h2 class='winMsg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+                            blocker();
+                        }
+                    }
+                });
+            } else {
+                looseCount += 1;
+                drawMan(looseCount);
+                if (looseCount == maxLooseCount) {
+                    resultText.innerHTML = `<h2 class='loseMsg'>You Lose!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+                    blocker();
+                }
+            }
+            button.disabled = true;
+        });
+        letterContainer.append(button);
+    }
+}
+
+function handleInput(key) {
+    var name =key.key;
+    var id  = name.charCodeAt(0) - 32;
+    console.log(name);
+    console.log(id);
+    if (id >= 65 && id < 91){
+      var button = document.getElementById('letter' + id);
+      button.click();
+      console.log("Clicked");
+    }
+  }
--- js_files/main.js
+++ js_files/main.js
@@ -1,5 +1,4 @@
 // Initialize references
-
 const letterContainer = document.getElementById("letterContainer");
 const userInput = document.getElementById("userInput");
 const newGameContainer = document.getElementById("newGameContainer");
@@ -14,38 +13,8 @@ let maxLooseCount = 0;
 let winCount = 0;
 let chosenWord = "";
 
-// Block all the buttons
-const blocker = () => {
-  let letterButtons = document.querySelectorAll(".letters");
-  letterButtons.forEach((button) => {
-    button.disabled.true;
-  });
-  newGameContainer.classList.remove("hide");
-};
-
-
-// Random Word Generator from the given options
-const generateWord = (levelInp) => {
-  // console.log(levels["Easy"]);
-  level = levelInp;
-
-  letterContainer.classList.remove("hide");
-  userInput.innerText = "";
-  let wordArray = levels[level]["words"];
-  maxLooseCount = levels[level]["attempts"];
-
-  // console.log(wordArray);
-  // console.log(maxLooseCount);
-
-  chosenWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-  chosenWord = chosenWord.toUpperCase();
-  let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
-  userInput.innerHTML = displayItem;
-
-};
-
 // Game Engine
-const initializer = () => {
+function initializer(){
   winCount = 0;
   looseCount = 0;
 
@@ -54,38 +23,8 @@ const initializer = () => {
   newGameContainer.classList.add("hide");
   letterContainer.innerHTML = "";
 
-  //For creating letter buttons
-  for (let i = 65; i < 91; i++) {
-    let button = document.createElement("button");
-    button.classList.add("letters");
-    button.innerText = String.fromCharCode(i);
-    button.setAttribute('id', 'letter' + i);
-    button.addEventListener("click", () => {
-      let charArray = chosenWord.split("");
-      let dashes = document.getElementsByClassName("dashes");
-      if (charArray.includes(button.innerText)) {
-        charArray.forEach((char, index) => {
-          if (char === button.innerText) {
-            dashes[index].innerText = char;
-            winCount += 1;
-            if (winCount == charArray.length) {
-              resultText.innerHTML = `<h2 class='winMsg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`;
-              blocker();
-            }
-          }
-        });
-      } else {
-        looseCount += 1;
-        drawMan(looseCount);
-        if (looseCount == maxLooseCount) {
-          resultText.innerHTML = `<h2 class='loseMsg'>You Lose!!</h2><p>The word was <span>${chosenWord}</span></p>`;
-          blocker();
-        }
-      }
-      button.disabled = true;
-    });
-    letterContainer.append(button);
-  }
+  createLetterButtons();
+
   let { initialDrawing } = canvasCreator();
   initialDrawing();
   window.addEventListener('keydown', handleInput, false);
@@ -93,16 +32,9 @@ const initializer = () => {
 
 
 newGameButton.addEventListener("click", initializer);
-window.onload = initializer;
-
-function handleInput(key) {
-  var name =key.key;
-  var id  = name.charCodeAt(0) - 32;
-  console.log(name);
-  console.log(id);
-  if (id >= 65 && id < 91){
-    var button = document.getElementById('letter' + id);
-    button.click();
-    console.log("Clicked");
-  }
+window.onload = () => {
+  initializer();
+  easyButton = document.getElementById("btnradio1");
+  easyButton.click();
 }
+
