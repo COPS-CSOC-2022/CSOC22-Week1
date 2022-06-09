c140d9bfe96187ac8c8f08402d0278dc8d0677c3
DecryptAG <akshat.goel.civ21@itbhu.ac.in>
2022-06-09 23:33:51 +0530

    final commit

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
-let display = document.getElementById('display');
-
+let screen = document.getElementById('screen');
 let buttons = Array.from(document.getElementsByClassName('buttons'));
 let drg = "D";
+let icon = document.getElementById("Mode");
+var historyData = [];
+var expressionData = "";
+var resultData = "";
+
+// For toggling the theme mode
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "sun.png";
+    }
+    else
+    {
+        icon.src = "moon.png";
+    }
+}
+
+//Main app logic
 buttons.map( btn => {
     btn.addEventListener('click', (e) => {
         switch(e.target.innerText){
             case 'AC':
-                display.innerText = '';
+                screen.innerText = "";
                 break;
-            case 'C':
-                if(display.innerText)
+            case 'DEL':
+                if(screen.innerText)
                 {
-                    display.innerText = display.innerText.slice(0,-1);
+                    screen.innerText = screen.innerText.slice(0,-1);
                     break;
                 }
             case 'DRG':
@@ -35,17 +52,18 @@ buttons.map( btn => {
                 }
                 break;
             case '+/-':
-                var x = display.innerText;
-                display.innerText = -1*(x);
+                var x = screen.innerText;
+                screen.innerText = -1*(x);
                 break;
             case 'e^x':
-                var x = display.innerText;
-                display.innerText = Math.exp(x);
+                var x = screen.innerText;
+                screen.innerText = Math.exp(x);
+                resultData = Math.exp(x);
                 break;
             case 'sin':
-                console.log(display.innerText);
-                var x = display.innerText;
-                if(drg == "R")
+                var x = screen.innerText;
+                expressionData = "sin("+x+")";
+                if(drg == "D")
                 {   
                     x = x* Math.PI/180;
                 }
@@ -53,23 +71,35 @@ buttons.map( btn => {
                 {
                     x = x*1.111111
                 }
-                display.innerText = Math.sin(x);
+                screen.innerText = Math.sin(x);
+                resultData = Math.sin(x);
+                historyData.push({"expression": expressionData, "result": resultData});
+                showlogdata();
+                expressionData="";
+                resultData="";
                 break;
             case 'cos':
-                var x = display.innerText;
-                if(drg == "R")
+                var x = screen.innerText;
+                expressionData = "cos("+x+")";
+                if(drg == "D")
                 {   
-                    x = x* Math.PI/180;
+                    x = x*Math.PI/180;
                 }
                 if(drg == "G")
                 {
                     x = x*1.111111
                 }
-                display.innerText = Math.cos(x);
+                screen.innerText = Math.cos(x);
+                resultData = Math.cos(x);
+                historyData.push({"expression": expressionData, "result": resultData});
+                showlogdata();
+                expressionData="";
+                resultData="";
                 break;
             case 'tan':
-                var x = display.innerText;
-                if(drg == "R")
+                var x = screen.innerText;
+                expressionData = "tan("+x+")";
+                if(drg == "D")
                 {   
                     x = x* Math.PI/180;
                 }
@@ -77,26 +107,49 @@ buttons.map( btn => {
                 {
                     x = x*1.111111
                 }
-                display.innerText = Math.tan(x);
+                screen.innerText = Math.tan(x);
+                resultData = Math.tan(x);
+                historyData.push({"expression": expressionData, "result": resultData});
+                showlogdata();
+                expressionData="";
+                resultData="";
                 break;
             case 'log':
-                var x = display.innerText;
-                display.innerText = Math.log(x);
+                var x = screen.innerText;
+                expressionData = "log("+x+")";
+                screen.innerText = Math.log(x);
+                historyData.push({"expression": expressionData, "result": resultData});
+                showlogdata();
+                expressionData="";
+                resultData="";
                 break;
             case '=':
+                expressionData = screen.innerText ;
                 try{
-                    display.innerText = eval(display.innerText);
+                    screen.innerText = eval(screen.innerText);
+                    resultData = eval(screen.innerText);
+                    historyData.push({"expression": expressionData, "result": resultData});
+                    showlogdata();
+                    expressionData="";
+                    resultData="";
                     break;
                 }
                 catch{
-                    display.innerText = "Error !!!";
+                    screen.innerText = "Error !!!";
                     break;
                 }
-                
             default: 
-                display.innerHTML += e.target.innerText;
+                screen.innerHTML += e.target.innerText;
         }
     })
-}
+})
 
-)
+//Function for displaying history
+function showlogdata(){
+    var log = document.getElementById("history-log");
+    var string="";
+    for(var key in historyData){
+        string+=" "+historyData[key]["expression"]+"="+historyData[key]["result"]+"<br>";
+    }
+    log.innerHTML=string;
+}
