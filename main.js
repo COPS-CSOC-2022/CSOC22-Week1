ccf7e3758285602eeab770e6d38cb6aa21d6ade7
DecryptAG <akshat.goel.civ21@itbhu.ac.in>
2022-06-09 09:01:41 +0530

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let display = document.getElementById('display');
 
 let buttons = Array.from(document.getElementsByClassName('buttons'));
-
+let drg = "D";
 buttons.map( btn => {
     btn.addEventListener('click', (e) => {
         switch(e.target.innerText){
@@ -14,6 +14,75 @@ buttons.map( btn => {
                     display.innerText = display.innerText.slice(0,-1);
                     break;
                 }
+            case 'DRG':
+                if(drg == "D")
+                {
+                    drg = "R"
+                    document.getElementById('drg').innerText =  drg;
+                    console.log("changed");
+                }
+                else if (drg == "R")
+                {
+                    drg = "G"
+                    document.getElementById('drg').innerText =  drg;
+                    console.log("changed");
+                }
+                else if(drg == "G")
+                {
+                    drg = "D";
+                    document.getElementById('drg').innerText =  drg;
+                    console.log("changed");
+                }
+                break;
+            case '+/-':
+                var x = display.innerText;
+                display.innerText = -1*(x);
+                break;
+            case 'e^x':
+                var x = display.innerText;
+                display.innerText = Math.exp(x);
+                break;
+            case 'sin':
+                console.log(display.innerText);
+                var x = display.innerText;
+                if(drg == "R")
+                {   
+                    x = x* Math.PI/180;
+                }
+                if(drg == "G")
+                {
+                    x = x*1.111111
+                }
+                display.innerText = Math.sin(x);
+                break;
+            case 'cos':
+                var x = display.innerText;
+                if(drg == "R")
+                {   
+                    x = x* Math.PI/180;
+                }
+                if(drg == "G")
+                {
+                    x = x*1.111111
+                }
+                display.innerText = Math.cos(x);
+                break;
+            case 'tan':
+                var x = display.innerText;
+                if(drg == "R")
+                {   
+                    x = x* Math.PI/180;
+                }
+                if(drg == "G")
+                {
+                    x = x*1.111111
+                }
+                display.innerText = Math.tan(x);
+                break;
+            case 'log':
+                var x = display.innerText;
+                display.innerText = Math.log(x);
+                break;
             case '=':
                 try{
                     display.innerText = eval(display.innerText);
